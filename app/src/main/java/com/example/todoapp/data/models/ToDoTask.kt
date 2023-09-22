0ddf6ee63237b2baf7fc63a10b5ffd1627cfce82
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-09-22 23:25:40 +0300

    create model class

---
+++ app/src/main/java/com/example/todoapp/data/models/ToDoTask.kt
@@ -0,0 +1,18 @@
+package com.example.todoapp.data.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.todoapp.util.Constant.DATABASE_TABLE
+
+
+@Entity(tableName = DATABASE_TABLE)
+data class ToDoTask(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title : String,
+    val description:String,
+    val priority : Priority
+
+
+
+)
---
+++ app/src/main/java/com/example/todoapp/util/Constant.kt
@@ -0,0 +1,8 @@
+package com.example.todoapp.util
+
+object Constant {
+
+    const val DATABASE_TABLE = "todo_table"
+    const val DATABASE_NAME = "todo_database"
+
+}
